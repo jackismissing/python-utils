7aa5c20f17a5d02c2cad473864820cb7f697ebc5
jackismissing <nicolas.j.garnier@gmail.com>
2014-03-14 17:50:23 +0100

    Integer to binary

---
+++ int-to-binary/tobin.py
@@ -0,0 +1,27 @@
+'''
+Converts a positive integer into a binary number
+Usage : num = ToBin(your_number)
+'''
+class ToBin():
+	def __init__(self, num):
+		self.convert(num)
+	
+	def convert(self, num):
+		result = ""
+		try:
+			num = int(num)
+			if num < 0:
+				try_again = raw_input('Please enter a valid positive integer: ')		
+				self.convert(try_again)
+			elif num == 0:
+				result = "0"
+			else:
+				while num > 0:
+					result = str(num%2) + result
+					num = num/2
+		except ValueError:	
+			try_again = raw_input('Please enter a valid positive integer: ')
+			self.convert(try_again)				
+		print result
+	
+
